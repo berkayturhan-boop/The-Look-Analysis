c8d5633ae486c4c2c9cf2c8f7a13e905e95b1625
R. Berkay Turhan <r.berkayturhan@gmail.com>
2026-01-02 14:38:13 +0300

    Delete profit_by_year_month.sql

--- profit_by_year_month.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-FORMAT_DATE('%Y-%m', DATE(created_at)) AS year_month,
-round(SUM(profit),2) as total_profit
-
- FROM  {{ref("int_orders_products")}} where status = "Complete"
- group by year_month order by year_month desc
