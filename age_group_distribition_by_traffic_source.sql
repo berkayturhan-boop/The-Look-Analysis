634c2d5eb57b4c714cbe8bd1e01340dd374167c7
R. Berkay Turhan <r.berkayturhan@gmail.com>
2026-01-02 14:38:56 +0300

    Delete age_group_distribition_by_traffic_source.sql

--- age_group_distribition_by_traffic_source.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  traffic_source,
-  age_group,
-  count(*) nb_of_users,
-  ROUND( count(*) / SUM( count(*)) OVER (PARTITION BY traffic_source) * 100, 2) AS share_of_group,
-  SUM( count(*)) OVER (PARTITION BY traffic_source) as total_traffic_source
-FROM {{ref("int_customer_segment")}}
-group by traffic_source, age_group
-ORDER BY traffic_source, age_group
